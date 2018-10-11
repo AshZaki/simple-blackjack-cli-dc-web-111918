34112614bbecf6cca530181420387aa71a614841
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-11 20:58:08 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  puts card1 = deal_card
-  puts card2 = deal_card
+  card1 = deal_card
+  card2 = deal_card
   sum = card1 + card2
   display_card_total(sum)
 end
